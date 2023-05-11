32e20eb0d153b3f8e53689f9050f0d06a0194978
Rohit Routh <49668692+RohitRouth@users.noreply.github.com>
2023-05-11 18:40:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+let bandNames = ['The Beatles', 'Aerosmith', 'Led Zeppelin', 'The Who', 'Nirvana', 'Rolling Stones', 'Pink Floyd'];
+
+// Remove articles from band names
+let regex = /^(a |an |the )/i;
+let sortedBands = bandNames.map(band => band.replace(regex, '').trim()).sort();
+
+// Get ul element and add li elements for each band name
+let ul = document.getElementById('band');
+sortedBands.forEach(band => {
+  let li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
+});
 
